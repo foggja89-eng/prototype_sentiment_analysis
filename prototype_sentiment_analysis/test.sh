27f5ceccbec70f18ef9fa9ad5121687feca2b7eb
foggja89-eng <foggja89@students.rowan.edu>
2026-02-06 21:33:47 -0500

    updated code and output for correct version number and added new test script

---
+++ prototype_sentiment_analysis/test.sh
@@ -0,0 +1,42 @@
+clear
+echo 'formatting java files'
+echo
+astyle --mode=java *.java
+echo
+echo 'compiling java files'
+echo
+javac *.java
+echo
+echo 'starting test -- positive sentiment'
+echo 
+java Driver.java < test1.txt > positive_output.txt
+echo 
+echo 'starting test -- neutral sentiment'
+echo 
+java Driver.java < test2.txt > neutral_output.txt
+echo 
+echo 'starting test -- negative sentiment'
+echo 
+java Driver.java < test3.txt > negative_output.txt 
+echo 
+echo 'unifying the results in output.txt'
+echo
+echo 'positive test' > unified_output.txt
+cat positive_output.txt >> unified_output.txt
+echo ' ' >> unified_output.txt
+echo 'neutral test ' >> unified_output.txt
+cat neutral_output.txt >> unified_output.txt
+echo ' ' >> unified_output.txt
+echo 'negative test' >> unified_output.txt
+cat negative_output.txt >> unified_output.txt
+echo ' ' >> unified_output.txt 
+echo 
+echo 'testing complete! the individual files can be viewed as positive_output.txt, neutral_output.txt, and negative_output.txt. Or, you can view it all together in unified_output.txt'
+echo
+echo 'current directory and files --'
+echo
+pwd
+echo
+ls
+echo
+echo '=====END OF TEST.SH====='
